bc0b5c27c5e7e3e8f37c029e72e50d58d6f57a67
0ldm0s <oldmos@gmail.com>
2022-06-12 00:43:30 +0800

    we not like docker

--- run_circusd.sh
+++ run_circusd.sh
@@ -5,4 +5,7 @@ cd $(dirname $0)
 work_path=$(pwd)
 mkdir -p "${work_path}/logs"
 ulimit -n 512000
-${PYTHON_ROOT}/circusd --daemon circus.ini
+# this for docker user
+#${PYTHON_ROOT}/circusd --daemon circus.ini
+# this for systemd user
+${PYTHON_ROOT}/circusd circus.ini
