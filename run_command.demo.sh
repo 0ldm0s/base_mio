63f168b08969c775af19b3fa10906a3ae52f7453
0ldm0s <oldmos@gmail.com>
2022-06-22 12:50:33 +0800

    fix script error

--- run_command.demo.sh
+++ run_command.demo.sh
@@ -9,5 +9,5 @@ export MIO_LIMIT_CPU=1
 cd $(dirname $0)
 mkdir -p logs
 # You can use the nohup to run in backgroup
-#nohup ${PYTHON_ROOT}/flask cli exe -cls=${EXECUTION} > "${work_path}\logs\cron.log" 2>&1 &
+#nohup ${PYTHON_ROOT}/flask cli exe -cls=${EXECUTION} &> "${work_path}/logs/cron.log" 2>&1 &
 ${PYTHON_ROOT}/flask cli exe -cls=${EXECUTION}
