a2dff08b58b623994285ec9a1c19ed37559a9f3d
0ldm0s <oldmos@gmail.com>
2022-09-22 19:20:07 +0800

    调整为支持pyc以及自动检测cpu数量

--- run_zerg.sh
+++ run_zerg.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 PYENV_ROOT="/usr/local/.pyenv"
 PYTHON_ROOT="$PYENV_ROOT/shims"
+UWSGI_NUM_PROCESSES=`grep -c ^processor /proc/cpuinfo`
 export MIO_CONFIG=production
 export PYTHONUNBUFFERED=1
 cd $(dirname $0)
 work_path=$(pwd)
 mkdir -p "${work_path}/logs"
 ulimit -n 512000
-uwsgi --wsgi-file mio/pymio-uwsgi.py --tornado 100 --greenlet \
-      --zerg /dev/shm/mutalisk --enable-threads -lazy-apps \
-      --master --thunder-lock --processes=8
+uwsgi --wsgi mio.pymio-uwsgi --tornado 100 --greenlet \
+      --zerg /dev/shm/mutalisk --enable-threads \
+      --master --thunder-lock --processes=$UWSGI_NUM_PROCESSES
